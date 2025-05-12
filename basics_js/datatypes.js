f04e5e61d779e713fd8cd888c39f6105a9d95ed7
ashishkdeshmukh31 <110197119+ashishkdeshmukh31@users.noreply.github.com>
2025-05-12 09:41:17 +0000

    datatypes

--- basics_js/datatypes.js
+++ basics_js/datatypes.js
@@ -1,21 +1,21 @@
-let na="Ashish"
-let age=20
-let s=null
-let u;
-console.log(typeof na)
-console.log(na,age,s,u)
-console.log(typeof s)  // object is the datatype for the null not for undefined
-console.log(typeof u)  //undefined
+// let na="Ashish"
+// let age=20
+// let s=null
+// let u;
+// console.log(typeof na)
+// console.log(na,age,s,u)
+// console.log(typeof s)  // object is the datatype for the null not for undefined
+// console.log(typeof u)  //undefined
 
 // to convert in another datatypes
-let n1="122"  //as a string
-console.log("Datatype Before:",typeof n1)
-let n2=Number(n1)
-console.log("Datatype After:",typeof n2)
+// let n1="122"  //as a string
+// console.log("Datatype Before:",typeof n1)
+// let n2=Number(n1)
+// console.log("Datatype After:",typeof n2)
 
-let isCheck=""  //not even space
-let convert=Boolean(isCheck)
-console.log(convert)
+// let isCheck=""  //not even space
+// let convert=Boolean(isCheck)
+// console.log(convert)
 
 /*  
 "33"=>33
@@ -24,6 +24,10 @@ true=>1;  false=>0
 1=>true; 0=>false
 " "=>(space)true;     ""=>false;    "ash"=>true
 */
-let str=33  //number
-let s1=String(str)
-console.log(typeof s1)
+// let str=33  //number
+// let s1=String(str)
+// console.log(typeof s1)
+
+console.log("2"==2)   // true it check only value
+console.log("2"===2)  // false  it check value as well as datatype
+
