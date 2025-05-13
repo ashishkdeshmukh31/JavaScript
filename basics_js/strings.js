85f05e28c6ff82591447eca536d371ca445e23d2
ashishkdeshmukh31 <110197119+ashishkdeshmukh31@users.noreply.github.com>
2025-05-13 09:09:17 +0000

    strings

---
+++ basics_js/strings.js
@@ -0,0 +1,9 @@
+//for primitive datatypes it uses stack (it passes as a copy)
+//for non primitive datatypes it uses heap (it passes the reference)
+
+let s="Ashish"
+console.log(s.charAt(3))  //it gives the letter on that perticular location
+console.log(s.indexOf('A'))  //it gives the index for that particular letter
+console.log(s.substring(0,3))  //it gives the part of the word
+console.log(s.slice(-3))  // we can print the reverse it takes -ve value as parameter
+ 
