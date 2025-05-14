b6f9e84165bbf4dc9b42ee0cf553f991ee43876d
ashishkdeshmukh31 <110197119+ashishkdeshmukh31@users.noreply.github.com>
2025-05-14 13:11:50 +0000

    strings

--- basics_js/strings.js
+++ basics_js/strings.js
@@ -6,4 +6,12 @@ console.log(s.charAt(3))  //it gives the letter on that perticular location
 console.log(s.indexOf('A'))  //it gives the index for that particular letter
 console.log(s.substring(0,3))  //it gives the part of the word
 console.log(s.slice(-3))  // we can print the reverse it takes -ve value as parameter
- 
+
+let str="  Tejas   "
+console.log(str)
+console.log(str.trim())
+
+const t="Ashish-Krishnat-Deshmukh"
+console.log(t.split('-'))
+
+console.log(`My name is ${s} and my brother name is ${str}`)
